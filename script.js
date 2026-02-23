133fd6757c8bf23dd78d9d9720a6f4a6b5bbec12
Neveslv <enzoematheus092009@gmail.com>
2026-02-23 12:35:00 -0300

    fix: resolver conflitos de merge

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const pShowMessage = document.getElementById("show-message")
+  const btn = document.getElementById("more-informton")
+
+  function sendMail() {
+    const email = prompt("Informe seu email");
+    (!email) ? pShowMessage.textContent = "Email não informado" : pShowMessage.textContent = `Email informado: ${email}`;
+  }
+
+  btn.addEventListener("click", sendMail)
+})
